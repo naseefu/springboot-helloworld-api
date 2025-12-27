43f4699913fb42a68b1dc796af0d815110ce53fe
naseefu <naseefrahman90@gmail.com>
2025-12-27 21:37:29 +0530

    updates inside rest controller

--- src/main/java/com/hello_world/api/controllers/HelloWorldController.java
+++ src/main/java/com/hello_world/api/controllers/HelloWorldController.java
@@ -1,15 +1,18 @@
 package com.hello_world.api.controllers;
 
 import com.hello_world.api.HelloWorldReponse;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 // this is the annotation which means that we're ready to use spring mvc to handle web request
 // @RestController =  @Controller + @ResponseBody
 public class HelloWorldController {
+
+    // /users/{userId}
+    @GetMapping("/hello/{name}/world")
+    public HelloWorldReponse helloWithPathVariable(@PathVariable String name){
+        return new HelloWorldReponse("Hello "+name+"!");
+    }
 
     @GetMapping("/hello")
     public HelloWorldReponse hello(){
