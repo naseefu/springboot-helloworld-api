34f23bee7bd067eece436130cbebec486636eb6b
naseefu <naseefrahman90@gmail.com>
2025-12-27 20:31:49 +0530

    added rest controller for hello world

--- src/main/java/com/hello_world/api/controllers/HelloWorldController.java
+++ src/main/java/com/hello_world/api/controllers/HelloWorldController.java
@@ -1,4 +1,16 @@
 package com.hello_world.api.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+// this is the annotation which means that we're ready to use spring mvc to handle web request
+// @RestController =  @Controller + @ResponseBody
 public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World!";
+    }
+
 }
